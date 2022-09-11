db1b9953cf1c0acd2724276049877faaa844ff43
Rhfialyndra <rahfialyendra5@gmail.com>
2022-09-11 22:11:36 +0700

    finished building assignment1 web

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_catalog(request):
+    context = {
+        'item_list' : CatalogItem.objects.all(),
+        'full_name' : 'Rahfi Alyendra G',
+        'student_id' : '2106705764',
+    }
+    return render(request, 'katalog.html', context);
